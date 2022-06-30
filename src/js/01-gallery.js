1cc1dfd1b6f2d5f263186a6457bf2ccea9f1874e
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-06-30 18:46:39 +0300

    Задание 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,37 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 console.log(galleryItems);
+
+const paletteContainer = document.querySelector(".gallery");
+const cardsMarkup = createColorCardsMarkup(galleryItems);
+
+paletteContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createColorCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+        <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+
+var lightbox = new SimpleLightbox('.gallery a');
+
+  lightbox.show();
