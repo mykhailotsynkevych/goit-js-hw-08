042d2e185d9f101ad3f4faf29dc24fa25b8d1cce
Mykhailo Tsynkevych <99801093+mykhailotsynkevych@users.noreply.github.com>
2022-07-02 15:38:03 +0300

    02-video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+
+const player = new Player(iframe);
+
+const onPlay = function (data) {
+    // console.dir(data.seconds)
+    const timeVideo = data.seconds;
+
+    localStorage.setItem('videoplayer-current-time', timeVideo)
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const getSecond = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(getSecond);
